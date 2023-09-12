5321495494400db0005045c46a924e1cf09ad2e7
IYStoychev21 <IYStoychev21@codingburgas.bg>
2023-09-12 12:51:40 +0300

    Add way to read config file

--- src/token_interpreter.rs
+++ src/token_interpreter.rs
@@ -1,10 +1,17 @@
 use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+use serde_yaml::{self};
 
 /*
  * -g (generate) - generate config file (copy configs to specified directory)
  * -dd (dotfiles directory) - directory where the dotfiles are stored 
  * -l (link) - does the symboly links between the dotfiles directories and the config directories
  * */
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    config_paths: Vec<String>
+}
 
 pub fn interprete(tokens: Vec<HashMap<String, String>>) -> () {
     let mut paths: HashMap<String, String> = HashMap::new();
@@ -20,4 +27,7 @@ pub fn interprete(tokens: Vec<HashMap<String, String>>) -> () {
     }
 
     println!("{:?}", paths);
+    let file = std::fs::File::open(paths["config"].to_string()).expect("Could not open file.");
+    let paths: Config = serde_yaml::from_reader(file).expect("Could not read values.");
+    println!("{:?}", paths.config_paths);
 }
