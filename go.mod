2aa462e67ec5603b30e12a2c7322a2abe3df1b81
Ivan Stoychev <IYStoychev21@codingburgas.bg>
2024-10-11 15:44:01 +0300

    Init go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module moonlight
+
+go 1.23.2
--- src/token_interpreter/config_files_backup.rs
+++
@@ -1,8 +0,0 @@
-use std::process::Command;
-use whoami;
-
-pub fn backup(for_backup: String, dotfile_directory: String) -> () {
-    let new_str: &str = &(for_backup.clone()[1..]);
-    let formated: String = format!("/home/{}{}", whoami::username(), new_str);
-    Command::new("cp").args(["-r", &formated, &dotfile_directory]).spawn().expect("failed to launch process");
-}
